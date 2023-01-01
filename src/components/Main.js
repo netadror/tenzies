4ab8241a93c2a234ce7dcc0394c7c29154c5c7de
Neta Dror <neta.l.dror@gmail.com>
2023-01-01 19:34:11 +0200

    added random die nums

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,30 @@
+import React from "react"
 import Die from "./Die"
+
 function Main() {
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    function allNewDice() {
+        const diceArray = []
+        for (let i = 0; i < 10; i++) {
+            let randNum = getRandomInt(1, 6)
+            diceArray.push(randNum)
+        }
+        console.log('diceArray', diceArray)
+        return diceArray
+    }
+    // console.log(allNewDice())
+    const [dice, setDice] = React.useState(allNewDice())
+    const diceElements = dice.map(die => <Die value={die} />)
+
     return (
         <main className="App-main">
             <div className='Die-container'>
-                <Die value="1" />
-                <Die value="2" />
-                <Die value="3" />
-                <Die value="4" />
-                <Die value="5" />
-                <Die value="1" />
-                <Die value="2" />
-                <Die value="8" />
-                <Die value="2" />
-                <Die value="4" />
+                {diceElements}
             </div>
         </main>
     )
