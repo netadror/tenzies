6f41aa45c5c9317ca35293b24cd76ac1b11b2b14
Neta Dror <neta.l.dror@gmail.com>
2023-01-03 17:25:15 +0200

    setup gh-pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,12 +25,3 @@ function Header(props) {
 
 }
 export default Header
-
-{/* <a
-className="App-link"
-href="https://reactjs.org"
-target="_blank"
-rel="noopener noreferrer"
->
-Learn React
-</a> */}
